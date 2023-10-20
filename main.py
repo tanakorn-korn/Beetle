521c07a02b0b738516f08610c2302962d0841db5
Your GitHub Username <your-email@example.com>
2023-10-20 16:22:07 +0700

    https://github.com/tanakorn-korn/Beetle.git

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print("Test")
+import pygame
+pygame.init()
 
+# กำหนดขนาดหน้าต่างเกม
+screen = pygame.display.set_mode((800, 600))
 
-print("Hellow git")
+# กำหนดชื่อหน้าต่าง
+pygame.display.set_caption("My Game")
+
+# ลูปหลักของเกม
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
